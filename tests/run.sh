436b033409d9d8cfe8090a4beebb300aaa9fd0ac
Igor Borodin <igborodi@microsoft.com>
2023-10-12 19:16:13 +0300

    Change run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -32,10 +32,10 @@ run_test decimals decimals
 run_test decimals decimals-l --logical-types
 run_test decimals-bytes decimals-bytes
 run_test decimals-bytes decimals-bytes-l --logical-types
-run_test columns columns-1 --columns '[\"a\"]'
-run_test columns columns-2 --columns '[\"b\"]'
-run_test columns columns-3 --columns '[\"a\", \"d\"]'
-run_test columns columns-4 --columns '[\"d\", \"a\"]'
+run_test columns columns-1 --columns "'[\"a\"]'"
+run_test columns columns-2 --columns "'[\"b\"]'"
+run_test columns columns-3 --columns "'[\"a\", \"d\"]'"
+run_test columns columns-4 --columns "'[\"d\", \"a\"]'"
 run_test dates dates-l --logical-types
 run_test datetimes datetimes-l --logical-types
 run_test escaping escaping
